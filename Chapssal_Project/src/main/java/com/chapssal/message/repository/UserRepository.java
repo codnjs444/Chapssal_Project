d1db166100a810480f0a5d8db128413d80587f8f
thsin <63122324+thsin@users.noreply.github.com>
2024-05-22 14:17:42 +0900

    Delete Chapssal_Project/src/main/java/com/chapssal/message directory
    메시지 디렉토리 다시 만들기

--- Chapssal_Project/src/main/java/com/chapssal/message/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chapssal.message.repository;
-
-import com.chapssal.message.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
