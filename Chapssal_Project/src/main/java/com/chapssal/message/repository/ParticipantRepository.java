b33642b3aa0343379d3356a060008eeb34388524
ldh <eogus0128@naver.com>
2024-05-28 20:05:22 +0900

    currentID

--- Chapssal_Project/src/main/java/com/chapssal/message/repository/ParticipantRepository.java
+++ Chapssal_Project/src/main/java/com/chapssal/message/repository/ParticipantRepository.java
@@ -3,6 +3,7 @@ package com.chapssal.message.repository;
 import com.chapssal.message.model.ChatRoom;
 import com.chapssal.message.model.Participant;
 import com.chapssal.user.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
     List<Participant> findByRoom_RoomNum(int roomNum);
 
+    @Transactional
     void deleteByRoomAndUser(ChatRoom room, User user);
 
 }
