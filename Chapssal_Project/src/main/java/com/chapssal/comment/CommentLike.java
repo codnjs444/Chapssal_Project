22d0cab3543e0dc9839bf057faf6a86748693eb6
808_dita <808_dita@DESKTOP-GNE71AK>
2024-05-31 14:28:39 +0900

    Merge remote-tracking branch 'origin/branch-ldh' into branch-lcw

---
+++ Chapssal_Project/src/main/java/com/chapssal/comment/CommentLike.java
@@ -0,0 +1,35 @@
+package com.chapssal.comment;
+
+import java.time.LocalDateTime;
+
+import com.chapssal.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CommentLike {
+ 
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private int likeNum;
+ 
+ @ManyToOne
+ @JoinColumn(name = "comment_id")
+ private Comment comment;
+ 
+ @ManyToOne
+ @JoinColumn(name = "user_id")
+ private User user;
+ 
+ private LocalDateTime likeDate;
+
+}
