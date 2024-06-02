0bf8dcb50c7ece1f1a46db22c470effc99744b9f
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-06-02 20:57:20 +0900

    대댓글 디자인 수정

--- Chapssal_Project/java/com/chapssal/comment/RCommentDTO.java
+++ Chapssal_Project/java/com/chapssal/comment/RCommentDTO.java
@@ -14,13 +14,15 @@ public class RCommentDTO {
     private String userName;
     private String text;
     private LocalDateTime date;
-
+    private String profilePictureUrl;
+    
     public RCommentDTO(RComment rComment) {
         this.rcommentNum = rComment.getRcommentNum();
         this.commentNum = rComment.getComment().getCommentNum();
         this.userNum = rComment.getUser().getUserNum();
         this.userName = rComment.getUser().getUserName();
         this.text = rComment.getText();
+        this.profilePictureUrl = rComment.getUser().getProfilePictureUrl();
         this.date = rComment.getDate();
     }
 }
