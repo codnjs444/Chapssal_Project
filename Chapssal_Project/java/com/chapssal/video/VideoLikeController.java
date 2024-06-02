87f4276d4f8a4996f957375fee79f373735b844a
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-06-02 21:58:00 +0900

    댓글 좋아요 + 영상 좋아요 비동기식 전달

--- Chapssal_Project/java/com/chapssal/video/VideoLikeController.java
+++ Chapssal_Project/java/com/chapssal/video/VideoLikeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/video")
@@ -22,15 +23,17 @@ public class VideoLikeController {
         videoLike.setLikeDate(LocalDateTime.now());
         videoLikeRepository.save(videoLike);
 
-        return ResponseEntity.ok().body("{\"success\": true}");
+        return ResponseEntity.ok().body("{\"success\": true, \"message\": \"좋아요를 눌렀습니다.\"}");
     }
 
     @DeleteMapping("/unlike")
     public ResponseEntity<?> unlikeVideo(@RequestBody VideoLikeRequest videoLikeRequest) {
-        VideoLike videoLike = videoLikeRepository.findByVideoAndUser(videoLikeRequest.getVideo(), videoLikeRequest.getUser());
-        if (videoLike != null) {
-            videoLikeRepository.delete(videoLike);
-            return ResponseEntity.ok().body("{\"success\": true}");
+        List<VideoLike> videoLikes = videoLikeRepository.findByVideoAndUser(videoLikeRequest.getVideo(), videoLikeRequest.getUser());
+        if (!videoLikes.isEmpty()) {
+            for (VideoLike videoLike : videoLikes) {
+                videoLikeRepository.delete(videoLike);
+            }
+            return ResponseEntity.ok().body("{\"success\": true, \"message\": \"좋아요를 취소하였습니다.\"}");
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"success\": false, \"message\": \"Like relationship not found.\"}");
         }
--- Chapssal_Project/java/com/chapssal/video/VideoLikeRepository.java
+++ Chapssal_Project/java/com/chapssal/video/VideoLikeRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface VideoLikeRepository extends JpaRepository<VideoLike, Integer> {
-	boolean existsByVideoAndUser(Integer video, Integer user);
-    VideoLike findByVideoAndUser(Integer video, Integer user);
+    boolean existsByVideoAndUser(Integer video, Integer user);
+    List<VideoLike> findByVideoAndUser(Integer video, Integer user); // 수정됨
     
     @Query("SELECT COUNT(vl) FROM VideoLike vl WHERE vl.video = :videoId")
     int countByVideoId(@Param("videoId") Integer videoId);
