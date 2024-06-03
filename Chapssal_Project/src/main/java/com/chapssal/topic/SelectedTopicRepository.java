ec1698cc6347fdab314218524d45aea0a4ce20d9
sth <calmest_@naver.com>
2024-06-03 10:52:50 +0900

    투표페이지 이번주만 검색/자동추천 수정완료

--- Chapssal_Project/src/main/java/com/chapssal/topic/SelectedTopicRepository.java
+++ Chapssal_Project/src/main/java/com/chapssal/topic/SelectedTopicRepository.java
@@ -2,6 +2,7 @@ package com.chapssal.topic;
 
 import com.chapssal.user.User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface SelectedTopicRepository extends JpaRepository<SelectedTopic, Integer> {
     List<SelectedTopic> findByUser(User user);
     List<SelectedTopic> findByTopic(Topic topic);
+    List<SelectedTopic> findAllByCreateDateBetween(LocalDateTime start, LocalDateTime end);
     
     @Query("SELECT st.topic, COUNT(st.topic) as topicCount FROM SelectedTopic st GROUP BY st.topic ORDER BY topicCount DESC")
     List<Object[]> findTopicsByVoteCount();
